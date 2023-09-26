6804a32e7b9aaf2ee0ce459f9a542695f149f002
norikois <33947244+norikois@users.noreply.github.com>
2023-09-26 14:10:02 +0900

    Add files via upload

---
+++ Data/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace KB11775_BlazorServerApp1.Data;
+
+public class Product
+{
+    public int ProductID { get; set; }
+    public String ProductName { get; set; } = "";
+    public int StockedAmount { get; set; }
+
+    public Product()
+    {
+    }
+}
