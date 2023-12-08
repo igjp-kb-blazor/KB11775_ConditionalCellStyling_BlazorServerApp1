37cbb5050d07f5cc5e299916db1e0c0cf8aeb28b
jsakamotoIGJP <jsakamotoIGJP@users.noreply.github.com>
2023-12-08 04:57:07 +0000

    deploy: fc3bbc361957c17ace7f06237c129283c4a5ceef

---
+++ IGJavaScript.js
@@ -0,0 +1,6 @@
+﻿igRegisterScript("CellClassesHandler", () => {
+    return {
+        low: (rowData, columnKey, cellValue, rowIndex) => rowData[columnKey] < 10,
+        high: (rowData, columnKey, cellValue, rowIndex) => rowData[columnKey] > 50
+    };
+}, true);
